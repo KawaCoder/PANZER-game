060cea5fb050133bcf1fcc4997ecb5bc705cc6ea
Xelax-XIII <gabax.barsav@gmail.com>
2024-02-06 19:05:23 +0100

    Canon qui peut tourner (sans création de TUC) Nouvelle image de canon

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from zombie_manager import zombie_manager
 from zombax import zombax
+from pygame.locals import *
 
 
 # Initialisation de Pygame
@@ -34,19 +35,20 @@ zombie_image = pygame.transform.scale(zombie_image, (zombie_width, zombie_height
 # Paramètre du canon 
 canon_image_path = "assets/canon/canon_tuyaux.png"
 canon_width, canon_height = 50, 100
-canon_pos = [25,height-floor_height-100]
+canon_pos = [75,height-floor_height-100]
 a = pygame.draw.rect(screen, (0,0,0), (150,150,150,150))
 
 
 # Chargement de l'image du canon
 canon_image = pygame.image.load(canon_image_path).convert_alpha()
+cannon_image = pygame.transform.rotate(canon_image, -80)
 rect_canon = canon_image.get_rect(center=(canon_pos))
 
 # Création de l'objet Clock
 clock = pygame.time.Clock()
 
 # paramètre du lancer
-alpha=360
+alpha=315
 vitesse_initiale_lancer=10
 
 
@@ -68,24 +70,33 @@ def rot_center(image, angle):
     rotate_rect.center = rotate_image.get_rect().center
     rotate_image = rotate_image.subsurface(rotate_rect).copy()
     return rotate_image
+canon_mouv = rot_center(canon_image, alpha)
 
 # Boucle principale
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and alpha<360:
-                alpha+=5
-                canon_image = rot_center(canon_image, 5)
-                
-            if event.key == pygame.K_RIGHT and alpha>270:
-                alpha-=5
-                canon_image = pygame.transform.rotate(canon_image, -5)
+
             if event.key == pygame.K_UP:
                 print(ZManager.getZombies())
             
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    keystate = pygame.key.get_pressed()
+
+    if keystate[K_LEFT]: # rotate conterclockwise
+        alpha+=2
+        if alpha > 360:
+            alpha = 360
+        canon_mouv = rot_center(canon_image, alpha)
+
+    if keystate[K_RIGHT]: # rotate clockwise
+        alpha-=2
+        if alpha < 270:
+            alpha = 270
+        canon_mouv = rot_center(canon_image, alpha)
 
     # Déplacer le zombie vers la gauche
     zombie_pos[0] -= zombie_speed
@@ -99,8 +110,8 @@ while True:
     pygame.draw.rect(screen, floor_color, (0, height - floor_height, width, floor_height))
 
     # Dessiner le canon
-    pygame.draw.rect(screen, canon_color, (0, height-floor_height-50, 100,50))
-    screen.blit(canon_image, rect_canon)
+    pygame.draw.rect(screen, canon_color, (50, height-floor_height-50, 100,50))
+    screen.blit(canon_mouv, rect_canon)
     
 
     # Afficher le zombie
